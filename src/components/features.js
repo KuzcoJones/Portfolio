fae4900ac88f13264d75cfe66271ddeeac8fcb97
Keenan Jones <keenanjones@keenans-air.attlocal.net>
2020-07-14 18:53:33 -0500

    Projects and features

--- src/components/features.js
+++ src/components/features.js
@@ -10,8 +10,9 @@ import {
 } from '@material-ui/core';
 
 import {GitHub, PlayArrow, } from '@material-ui/icons'
-// import classes from '*.module.css';
 import ShoutRandomly from '../images/SignupShout.jpeg'
+import Day21 from '../images/Day21 .png'
+import weather from '../images/weather-app.png'
 
 
 
@@ -20,7 +21,7 @@ const features = () => {
 
       return ( 
       <Grid className="features" spacing={2} justify="space-evenly">
-            <Card className = 'card'>
+            <Card class = 'card' >
                   <CardHeader title="ShoutRandomly"/>
                   <CardActionArea >
                         <CardMedia 
@@ -32,54 +33,47 @@ const features = () => {
                               
                   </CardActionArea> 
                   <CardActions>
-                        <IconButton className="feature-button">
-                              <PlayArrow />
+                        <IconButton className="feature-button" href="https://youtu.be/3adb9v1WIyA" target="_blank">
+                              <PlayArrow color="secondary"/>
                         </IconButton>
-                        <IconButton className="feature-button">
-                              <GitHub/>
+                        <IconButton className="feature-button" href="https://github.com/KuzcoJones/FE-ShoutRandomlyPart-III" target="_blank">
+                              <GitHub color="secondary"/>
                         </IconButton>
                   </CardActions>
             </Card>
             
 
-            <Card className = 'card'>
-                  <CardHeader title="ShoutRandomly"/>
+            <Card class = 'card'>
+                  <CardHeader title="Day21"/>
                   <CardActionArea >
                         <CardMedia 
                         component="img"
                         height="290"
-                        image={ShoutRandomly}
-                        title = "ShoutRandomly"
-                        / >
+                        image={Day21}
+                        title = "Day21"
+                        />
                               
                   </CardActionArea> 
                   <CardActions>
-                        <IconButton className="feature-button">
-                              <PlayArrow/>
-                        </IconButton>
-                        <IconButton className="feature-button">
-                              <GitHub/>
+                        <IconButton className="feature-button" href="https://github.com/KuzcoJones/FE-Day21" target="_blank">
+                              <GitHub color="secondary"/>
                         </IconButton>
                   </CardActions>
             </Card>
 
-            <Card className = 'card'>
-                  <CardHeader title="ShoutRandomly"/>
+            <Card class = 'card' >
+                  <CardHeader title="Weather Widget" color="primary"/>
                   <CardActionArea >
                         <CardMedia 
                         component="img"
                         height="290"
-                        image={ShoutRandomly}
-                        title = "ShoutRandomly"
-                        / >
-                              
+                        image={weather}
+                        title = "Weather Widget"
+                        />
                   </CardActionArea> 
                   <CardActions>
-                        <IconButton className="feature-button">
-                              <PlayArrow/>
-                        </IconButton>
-                        <IconButton className="feature-button">
-                              <GitHub/>
+                        <IconButton className="feature-button"  >
+                              <GitHub  color="secondary"/>
                         </IconButton>
                   </CardActions>
             </Card> 
