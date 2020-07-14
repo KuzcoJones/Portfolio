7bae24befff80ef795c34750dbf83f7fbe1346b4
Keenan Jones <keenanjones@keenans-air.attlocal.net>
2020-07-14 08:58:13 -0500

    Animations

--- src/components/work.js
+++ src/components/work.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Work} from '@material-ui/icons'
 import {Typography} from '@material-ui/core'
-
+import {Fade} from 'react-reveal'
 const work = () => {
  return (
   <div className= "education-experience" id="work">
-   <Work style={{ fontSize: 60, color: 'white' }} />
+   <Fade bottom><Work style={{ fontSize: 60, color: 'white' }} /></Fade>
     <Typography variant="subtitle1" className="experience-title">Experience</Typography> <hr/>
     <Typography variant="h6" id="freelance">Freelance Developer</Typography>
       <br/>
